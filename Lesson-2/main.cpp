84fee8719192c44380fb69494386755c4747377b
nix7drummer88 <bguise135@gmail.com>
2017-10-15 23:22:47 -0400

    Completed Lesson 2 - #include statements - using and namespaces

---
+++ Lesson-2/main.cpp
@@ -0,0 +1,12 @@
+// The preprocessor handles this include statement
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// Without "using" statement, this would be std::cout
+	cout << "Hi there!" << endl;	// "endl" = next line
+	return 0;
+}
+
