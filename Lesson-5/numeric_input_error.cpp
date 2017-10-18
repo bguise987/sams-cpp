b8fe06db4b15f9cf159ba2e711191c12ceea17d5
nix7drummer88 <bguise135@gmail.com>
2017-10-18 01:04:46 -0400

    Added initial Lesson 5 code on error checking

---
+++ Lesson-5/numeric_input_error.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int ReturnCode = 0;
+
+    float Dividend = 0;
+    cout << "Dividend: ";
+    cin >> Dividend;
+
+    if (!cin.fail()) // Dividend is a number
+    {
+        float Divisor = 1;
+        cout << "Divisor: ";
+        cin >> Divisor;
+
+        float Result = (Dividend/Divisor);
+        cout << Result << endl;
+    }
+    else    // Dividend is not a number
+    {
+        cerr << "Input error, not a number?" << endl;
+
+        cin.clear();    // Reset the input failure indicators
+        // Eat the bad input so we can pause the program
+        char BadInput[5];   // Up to 5 characters
+        cin >> BadInput;
+
+        ReturnCode = 1;
+    };
+
+    // Note: You must type something before the Enter key
+    char StopCharacter;
+    cout << endl << "Press a key and \"Enter\": ";
+    cin >> StopCharacter;
+
+    return ReturnCode;
+}
