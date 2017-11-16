4d2a06b53a8a371f6042691f64baf1870efd9c76
nix7drummer88 <bguise135@gmail.com>
2017-11-16 01:35:44 -0500

    Fixed typo (missing semicolon)

--- Lesson-10/PromptModule.cpp
+++ Lesson-10/PromptModule.cpp
@@ -43,7 +43,7 @@ namespace SAMSPrompt
                     (DoneCharacter == 'y')
                     ||
                     (DoneCharacter == 'n')
-                )
+                );
 
             if (InvalidCharacterWasEntered)
             {
